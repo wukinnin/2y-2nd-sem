ddc8c8fb60a9450ae444e0f397e2fff38597cb63
Nimu <nimugonzaga@protonmail.com>
2024-02-08 07:49:44 +0800

    add ex 1 & 2

---
+++ conDenom/conDenom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conDenom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cash;
+            Console.Write("Enter cash: ");
+            cash = int.Parse(Console.ReadLine());
+
+            int[] denominations = { 1000, 500, 200, 100, 50, 20, 10, 5, 1);
+            foreach(int denomination in denominations)
+            {
+                int count = amount / denomination;
+                Console.WriteLine($"{denomination} = {count}");
+                amount %= denomination;
+            }
+
+
+        }
+    }
+}
---
+++ winAreaRec/winAreaRec/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace winAreaRec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelArea = new System.Windows.Forms.Label();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Length";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(110, 32);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 2;
+            this.txtLength.TextChanged += new System.EventHandler(this.txtLength_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Width";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(110, 78);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Area of A Rectangle is";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // labelArea
+            // 
+            this.labelArea.AutoSize = true;
+            this.labelArea.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelArea.Location = new System.Drawing.Point(172, 179);
+            this.labelArea.Name = "labelArea";
+            this.labelArea.Size = new System.Drawing.Size(0, 14);
+            this.labelArea.TabIndex = 6;
+            this.labelArea.Click += new System.EventHandler(this.areaLabel_Click);
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Location = new System.Drawing.Point(110, 125);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(100, 23);
+            this.btnCompute.TabIndex = 7;
+            this.btnCompute.Text = "Compute Area";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 270);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.labelArea);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Area of A Rectangle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelArea;
+        private System.Windows.Forms.Button btnCompute;
+    }
+}
+
