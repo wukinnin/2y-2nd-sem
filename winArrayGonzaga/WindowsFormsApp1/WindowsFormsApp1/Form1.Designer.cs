7502fb1174a9fc0095d690a474643442fcfd4f80
Nimu <nimugonzaga@protonmail.com>
2024-03-19 16:28:37 +0800

    add practical mar 19

---
+++ winArrayGonzaga/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnGetValues = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnResetArray = new System.Windows.Forms.Button();
+            this.btnViewArray = new System.Windows.Forms.Button();
+            this.btnComputeMean = new System.Windows.Forms.Button();
+            this.btnComputeMode = new System.Windows.Forms.Button();
+            this.btnComputeMedian = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(15, 36);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(124, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnGetValues
+            // 
+            this.btnGetValues.Location = new System.Drawing.Point(165, 33);
+            this.btnGetValues.Name = "btnGetValues";
+            this.btnGetValues.Size = new System.Drawing.Size(75, 24);
+            this.btnGetValues.TabIndex = 1;
+            this.btnGetValues.Text = "Get Values";
+            this.btnGetValues.UseVisualStyleBackColor = true;
+            this.btnGetValues.Click += new System.EventHandler(this.getValuesButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please enter an integer value of size 10";
+            // 
+            // btnResetArray
+            // 
+            this.btnResetArray.Location = new System.Drawing.Point(327, 33);
+            this.btnResetArray.Name = "btnResetArray";
+            this.btnResetArray.Size = new System.Drawing.Size(75, 23);
+            this.btnResetArray.TabIndex = 4;
+            this.btnResetArray.Text = "Reset Array";
+            this.btnResetArray.UseVisualStyleBackColor = true;
+            this.btnResetArray.Click += new System.EventHandler(this.btnResetArray_Click);
+            // 
+            // btnViewArray
+            // 
+            this.btnViewArray.Location = new System.Drawing.Point(246, 33);
+            this.btnViewArray.Name = "btnViewArray";
+            this.btnViewArray.Size = new System.Drawing.Size(75, 23);
+            this.btnViewArray.TabIndex = 5;
+            this.btnViewArray.Text = "View Array";
+            this.btnViewArray.UseVisualStyleBackColor = true;
+            this.btnViewArray.Click += new System.EventHandler(this.btnViewArray_Click);
+            // 
+            // btnComputeMean
+            // 
+            this.btnComputeMean.Location = new System.Drawing.Point(15, 84);
+            this.btnComputeMean.Name = "btnComputeMean";
+            this.btnComputeMean.Size = new System.Drawing.Size(124, 23);
+            this.btnComputeMean.TabIndex = 6;
+            this.btnComputeMean.Text = "Compute Mean";
+            this.btnComputeMean.UseVisualStyleBackColor = true;
+            this.btnComputeMean.Click += new System.EventHandler(this.btnComputeMean_Click);
+            // 
+            // btnComputeMode
+            // 
+            this.btnComputeMode.Location = new System.Drawing.Point(148, 84);
+            this.btnComputeMode.Name = "btnComputeMode";
+            this.btnComputeMode.Size = new System.Drawing.Size(124, 23);
+            this.btnComputeMode.TabIndex = 7;
+            this.btnComputeMode.Text = "Compute Mode";
+            this.btnComputeMode.UseVisualStyleBackColor = true;
+            this.btnComputeMode.Click += new System.EventHandler(this.btnComputeMode_Click);
+            // 
+            // btnComputeMedian
+            // 
+            this.btnComputeMedian.Location = new System.Drawing.Point(278, 84);
+            this.btnComputeMedian.Name = "btnComputeMedian";
+            this.btnComputeMedian.Size = new System.Drawing.Size(124, 23);
+            this.btnComputeMedian.TabIndex = 8;
+            this.btnComputeMedian.Text = "Compute Median";
+            this.btnComputeMedian.UseVisualStyleBackColor = true;
+            this.btnComputeMedian.Click += new System.EventHandler(this.btnComputeMedian_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 130);
+            this.Controls.Add(this.btnComputeMedian);
+            this.Controls.Add(this.btnComputeMode);
+            this.Controls.Add(this.btnComputeMean);
+            this.Controls.Add(this.btnViewArray);
+            this.Controls.Add(this.btnResetArray);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetValues);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Mode, Mean, and Median";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnGetValues;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnResetArray;
+        private System.Windows.Forms.Button btnViewArray;
+        private System.Windows.Forms.Button btnComputeMean;
+        private System.Windows.Forms.Button btnComputeMode;
+        private System.Windows.Forms.Button btnComputeMedian;
+    }
+}
+
---
+++ winArrayGonzaga/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int size = 10;
+        int[] numbers = new int[size];
+        int currentIndex;
+
+        private void getValuesButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtInput.Text, out int value))
+            {
+                numbers[currentIndex] = value;
+                currentIndex++;
+                MessageBox.Show("Value " + value + " is added to the array.");
+                txtInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid value.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewArray_Click(object sender, EventArgs e)
+        {
+            string arrayContent = "Current Array Contents:\n";
+            for (int i = 0; i < currentIndex; i++)
+            {
+                int arrayNum = numbers[i];
+                arrayContent += arrayNum.ToString() + "\n";
+            }
+            MessageBox.Show(arrayContent, "Array Contents");
+        }
+
+        private void btnResetArray_Click(object sender, EventArgs e)
+        {
+            Array.Clear(numbers, 0, numbers.Length);
+            currentIndex = 0;
+            MessageBox.Show("Array cleared.");
+        }
+
+        // COMPUTATIONS
+
+        private void btnComputeMean_Click(object sender, EventArgs e)
+        {
+            ComputeMean();
+        }
+
+        private void ComputeMean()
+        {
+            if (currentIndex > 0)
+            {
+                int sum = 0;
+                for (int i = 0; i < currentIndex; i++)
+                {
+                    sum += numbers[i];
+                }
+                double mean = (double)sum / currentIndex;
+                MessageBox.Show("Mean: " + mean, "Mean");
+            }
+            else
+            {
+                MessageBox.Show("No values.", "Empty Array");
+            }
+        }
+
+        private void btnComputeMode_Click(object sender, EventArgs e)
+        {
+            ComputeMode();
+        }
+
+        private void ComputeMode()
+        {
+            if (currentIndex > 0)
+            {
+                Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+                foreach (int value in numbers)
+                {
+                    if (value != 0)
+                    {
+                        if (frequencyMap.ContainsKey(value))
+                        {
+                            frequencyMap[value]++;
+                        }
+                        else
+                        {
+                            frequencyMap[value] = 1;
+                        }
+                    }
+                }
+                int maxFrequency = frequencyMap.Values.Max();
+                List<int> modes = frequencyMap.Where(kv => kv.Value == maxFrequency).Select(kv => kv.Key).ToList();
+
+                // display message
+
+                string modeMessage;
+                if (modes.Count == 1)
+                {
+                    modeMessage = $"Mode: {modes[0]}";
+                }
+                else
+                {
+                    modeMessage = "Modes: " + string.Join(", ", modes);
+                }
+                MessageBox.Show(modeMessage, "Mode");
+            }
+            else
+            {
+                MessageBox.Show("No values.", "Empty Array");
+            }
+        }
+
+        private void btnComputeMedian_Click(object sender, EventArgs e)
+        {
+            ComputeMedian();
+        }
+
+        private void ComputeMedian()
+        {
+            if (currentIndex > 0)
+            {
+                Array.Sort(numbers, 0, currentIndex);
+
+                double median;
+                if (currentIndex % 2 == 0)
+                {
+                    int middleIndex1 = currentIndex / 2 - 1;
+                    int middleIndex2 = currentIndex / 2;
+                    median = (numbers[middleIndex1] + numbers[middleIndex2]) / 2.0;
+                }
+                else
+                {
+                    int middleIndex = currentIndex / 2;
+                    median = numbers[middleIndex];
+                }
+
+                MessageBox.Show($"Median: {median}", "Median");
+            }
+            else
+            {
+                MessageBox.Show("No values.", "Empty Array");
+            }
+        }
+
+
+
+    }
+}
